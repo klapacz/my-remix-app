7d7a529bebefb52682fd97ebd09b4d447ed98d6e
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-06-13 00:27:20 +0200

    chore: use loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,22 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+type LoaderData = {
+  greeting: string;
+};
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = { greeting: "Welcome to Remix" };
+  return json(data);
+};
+
 export default function Index() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div className="flex justify-center p-4">
-      <h1 className="text-2xl">Welcome to Remix</h1>
+      <h1 className="text-2xl">{data.greeting}</h1>
     </div>
   );
 }
